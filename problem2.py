2bd33c5c272fc66368b4d469499fd92e67ea735a
sharaddobariya05 <50762494+sharaddobariya05@users.noreply.github.com>
2021-11-04 04:25:15 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,10 +1,10 @@
 if __name__ == '__main__':
     n = int(input())
-
+# taken empty string
     string = ""
-
+# from starting index 1 to ending index n, we have concated in the string.
 for i in range(1, n+1):
-    temp = str(i)
+    temp = str(i) 
     string = string + temp
     
 print(string)
